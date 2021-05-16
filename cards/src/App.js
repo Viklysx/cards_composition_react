923e0e2c0d8e1f6f5e1ddc23a74a74da15f7122f
Viklysx <vikbelec111@yandex.ru>
2021-05-16 13:00:22 +0300

    component creation completed

--- cards/src/App.js
+++ cards/src/App.js
@@ -3,6 +3,7 @@ import Card from './components/Card';
 
 function App() {
   return (
+    <>
     <Card img='https://avatars.mds.yandex.net/get-zen_doc/176438/pub_5f9d216149505f68117bafc9_5f9d249949505f6811808c0a/scale_1200'>        
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
@@ -10,6 +11,14 @@ function App() {
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
     </Card>
+    <Card>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </Card>
+    </>
   );
 }
 
--- cards/src/components/Card.js
+++ cards/src/components/Card.js
@@ -1,9 +1,14 @@
+import PropTypes from 'prop-types';
 export default function Card(props) {
-    const Img = ({img}) => img ? <img src={img} className="card-img-top" alt="img-card" /> : '';
+    const ImgCard = ({img}) => img ? <img src={img} className="card-img-top" alt="img-card" /> : '';
     return (
         <div className="card">
-            <Img img={props.img} />
+            <ImgCard img={props.img} />
             {props.children}
         </div>
     )
+}
+
+Card.propTypes = {
+    children: PropTypes.node
 }
